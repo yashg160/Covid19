10533ff9060a876cb06643501ec8f19597174855
Yash Gupta <yashg160@gmail.com>
2020-04-08 23:59:19 +0530

    CSS classes updated

--- src/App.js
+++ src/App.js
@@ -9,39 +9,37 @@ export default class App extends React.Component {
 					<h3>COVID-19</h3>
 				</div>
 				<div className="container">
-					<div className="global">
-						<div className="global-card">
+					<div className="card-container">
+						<div className="card">
 							<p>Total Global Cases</p>
 							<h5>Some figure</h5>
 						</div>
 
-						<div className="global-card">
+						<div className="card">
 							<p>Total Global Deaths</p>
 							<h5>Some figure</h5>
 						</div>
 
-						<div className="global-card">
+						<div className="card">
 							<p>Total Global Cured</p>
 							<h5>Some figure</h5>
 						</div>
 					</div>
 
-					<div className="content">
-						<div className="numbers">
-							<div className="numbers-card">
-								<p>Total India Cases</p>
-								<h5>Some figure</h5>
-							</div>
+					<div className="card-container">
+						<div className="card">
+							<p>Total India Cases</p>
+							<h5>Some figure</h5>
+						</div>
 
-							<div className="numbers-card">
-								<p>Total India Cases</p>
-								<h5>Some figure</h5>
-							</div>
+						<div className="card">
+							<p>Total India Cases</p>
+							<h5>Some figure</h5>
+						</div>
 
-							<div className="numbers-card">
-								<p>Total India Cases</p>
-								<h5>Some figure</h5>
-							</div>
+						<div className="card">
+							<p>Total India Cases</p>
+							<h5>Some figure</h5>
 						</div>
 					</div>
 				</div>
