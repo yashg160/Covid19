8441cc948ba4a53b07fab5ab36a71db237fdee2a
Yash Gupta <yashg160@gmail.com>
2020-04-09 19:03:46 +0530

    Redux store and moved content to Covid

--- src/App.js
+++ src/App.js
@@ -1,51 +1,18 @@
 import React from "react";
-import AutocompleteText from "./AutocompleteText";
-import "./css/App.css";
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
+import Covid from "./Covid";
 
-export default class App extends React.Component {
+const store = ConfigureStore();
+
+class App extends React.Component {
 	render() {
 		return (
-			<div className="main">
-				<div id="navbar">
-					<h3>COVID-19</h3>
-				</div>
-				<div className="container">
-					<div className="card-container">
-						<div className="card">
-							<p>Total Global Cases</p>
-							<h5>Some figure</h5>
-						</div>
-
-						<div className="card">
-							<p>Total Global Deaths</p>
-							<h5>Some figure</h5>
-						</div>
-
-						<div className="card">
-							<p>Total Global Cured</p>
-							<h5>Some figure</h5>
-						</div>
-					</div>
-					<h2 className="instruction">Enter a country name</h2>
-					<AutocompleteText />
-					<div className="card-container">
-						<div className="card">
-							<p>Total India Cases</p>
-							<h5>Some figure</h5>
-						</div>
-
-						<div className="card">
-							<p>Total India Cases</p>
-							<h5>Some figure</h5>
-						</div>
-
-						<div className="card">
-							<p>Total India Cases</p>
-							<h5>Some figure</h5>
-						</div>
-					</div>
-				</div>
-			</div>
+			<Provider store={store}>
+				<Covid />
+			</Provider>
 		);
 	}
 }
+
+export default App;
