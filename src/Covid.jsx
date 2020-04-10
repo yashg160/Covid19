52f27caa058f3313a825df9000d7fe06612e42fd
Yash Gupta <yashg160@gmail.com>
2020-04-10 16:27:39 +0530

    Grid display classes added

--- src/Covid.jsx
+++ src/Covid.jsx
@@ -19,37 +19,37 @@ class Covid extends React.Component {
 				</div>
 				<div className="container">
 					<div className="card-container">
-						<div className="card">
-							<p>Total Global Cases</p>
-							<h5>Some figure</h5>
+						<div className="card card-1">
+							<p className="card-title">Total India Cases</p>
+							<h5 className="card-content">Some figure</h5>
 						</div>
 
-						<div className="card">
-							<p>Total Global Deaths</p>
-							<h5>Some figure</h5>
+						<div className="card card-2">
+							<p className="card-title">Total India Cases</p>
+							<h5 className="card-content">Some figure</h5>
 						</div>
 
-						<div className="card">
-							<p>Total Global Cured</p>
-							<h5>Some figure</h5>
+						<div className="card card-3">
+							<p className="card-title">Total India Cases</p>
+							<h5 className="card-content">Some figure</h5>
 						</div>
 					</div>
 					<h2 className="instruction">Enter a country name</h2>
 					<AutocompleteText />
 					<div className="card-container">
-						<div className="card">
-							<p>Total India Cases</p>
-							<h5>Some figure</h5>
+						<div className="card card-1">
+							<p className="card-title">Total India Cases</p>
+							<h5 className="card-content">Some figure</h5>
 						</div>
 
-						<div className="card">
-							<p>Total India Cases</p>
-							<h5>Some figure</h5>
+						<div className="card card-2">
+							<p className="card-title">Total India Cases</p>
+							<h5 className="card-content">Some figure</h5>
 						</div>
 
-						<div className="card">
-							<p>Total India Cases</p>
-							<h5>Some figure</h5>
+						<div className="card card-3">
+							<p className="card-title">Total India Cases</p>
+							<h5 className="card-content">Some figure</h5>
 						</div>
 					</div>
 				</div>
