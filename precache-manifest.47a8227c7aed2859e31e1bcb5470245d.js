945705e56818716eb0f47f61b090ca9035fdc415
Yash Gupta <yashg160@gmail.com>
2020-04-10 20:57:57 +0530

    Updates

---
+++ precache-manifest.47a8227c7aed2859e31e1bcb5470245d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f64089b431dbccd99e9f30ce172952c",
+    "url": "/Covid19/index.html"
+  },
+  {
+    "revision": "3adb47c9eafd5e2ea80c",
+    "url": "/Covid19/static/css/main.7ce5fb06.chunk.css"
+  },
+  {
+    "revision": "f26a488ffcbf02732419",
+    "url": "/Covid19/static/js/2.3ba06c0f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Covid19/static/js/2.3ba06c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3adb47c9eafd5e2ea80c",
+    "url": "/Covid19/static/js/main.950e61fc.chunk.js"
+  },
+  {
+    "revision": "d597ef8edb7a0b2a0114",
+    "url": "/Covid19/static/js/runtime-main.fb07a569.js"
+  }
+]);
