53c30970a0dbca168bfea92ed47478d01ad0c8b3
Yash Gupta <yashg160@gmail.com>
2020-04-12 15:08:08 +0530

    Created India reducer

---
+++ src/redux/India.js
@@ -0,0 +1,26 @@
+import * as ActionTypes from "./ActionTypes";
+
+export default function India(
+	state = {
+		errMess: "ERR_NONE",
+		loading: true,
+		data: null,
+	},
+	action
+) {
+	switch (action.type) {
+		case ActionTypes.LOADING_INDIA:
+			return { ...state, data: {} };
+		case ActionTypes.ADD_INDIA:
+			return { ...state, loading: false, data: action.payload };
+		case ActionTypes.ERROR_INDIA:
+			return {
+				...state,
+				loading: false,
+				data: action.payload,
+				errMess: "ERR_TRUE",
+			};
+		default:
+			return state;
+	}
+}
