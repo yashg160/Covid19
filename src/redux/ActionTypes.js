8fbc421a1216cbf6842c100799ce91ff0c126694
Yash Gupta <yashg160@gmail.com>
2020-04-12 15:07:43 +0530

    India specific types

--- src/redux/ActionTypes.js
+++ src/redux/ActionTypes.js
@@ -5,3 +5,7 @@ export const ERROR_GLOBAL = "ERROR_GLOBAL";
 export const LOADING_COUNTRY = "LOADING_COUNTRY";
 export const ADD_COUNTRY = "ADD_COUNTRY";
 export const ERROR_COUNTRY = "ERROR_COUNTRY";
+
+export const LOADING_INDIA = "LOADING_INDIA";
+export const ADD_INDIA = "ADD_INDIA";
+export const ERROR_INDIA = "ERROR_INDIA";
